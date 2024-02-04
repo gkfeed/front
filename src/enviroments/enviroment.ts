e1488316501ebc5d38f6e734321b14fee4ed2b8d
Gakawarstone <pinnekerv@gmail.com>
2024-02-04 20:29:07 +0100

    chore: change api root

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  api_root: 'http://gkfeed.gws/api/v1/',
+  api_root: 'http://feed.gws.freemyip.com/api/v1/',
   url_root: 'http://localhost:4200/',
 };
